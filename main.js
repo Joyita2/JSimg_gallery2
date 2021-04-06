f6185bf7928e4d4d14a93b3c04996d733f21fe0c
Joyita2 <76734262+Joyita2@users.noreply.github.com>
2021-04-06 13:41:07 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+
+var t1 = document.getElementById('title');
+t1.style.color ='green';
+t1.style.textTransform ='uppercase';
+t1.style.marginTop ='20px';
+t1.style.marginBottom ='40px';
+t1.style.textAlign ='center';
+
+/*var galaryDiv1 = document.getElementById('galaryDiv1');
+galaryDiv1.style.marginTop ='20px';
+galaryDiv1.style.textAlign ='center';
+
+var galaryDiv2 = document.getElementById('galaryDiv2');
+galaryDiv2.style.marginTop ='20px';
+galaryDiv2.style.textAlign ='center';*/
+
+/*function myFunction() {
+	var galary = document.getElementById('img1');
+     galary.src=
+}*/
